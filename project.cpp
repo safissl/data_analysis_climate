46057aca842b6df81187468ea7e273b4235068a8
safissl <safissl@gmail.com>
2018-11-09 23:27:15 +0100

    Uncomment the functions

--- project.cpp
+++ project.cpp
@@ -5,10 +5,10 @@ void project() {
 	string pathToFile = "smhi-opendata_Lund.csv"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
 
-	//t.DayTemp(8, 23); //Call some functions that you've implemented
+  t.DayTemp(8, 23); //Call some functions that you've implemented
 	//t.tempOnDay(235);
 	//t.tempPerDay();
-  //t.hotCold();
+  t.hotCold();
 	//t.tempPerYear(2050);
-  //t.tempPerMonth();
+  t.tempPerMonth();
 }
--- tempTrender.h
+++ tempTrender.h
@@ -11,7 +11,7 @@ class tempTrender {
   std::string getFilePath(){return filePath_;}
   double Gaussian(double* x, double* par); // fitting function for hottest and coldest day
 
-	//void DayTemp(int monthToCalculate, int dayToCalculate); //Make a histogram of the temperature on this day
+  void DayTemp(int monthToCalculate, int dayToCalculate); //Make a histogram of the temperature on this day
 	//void tempOnDay(int dateToCalculate); //Make a histogram of the temperature on this date
 	//void tempPerDay(); //Make a histogram of the average temperature of each day of the year
   void hotCold(); //Make a histogram of the hottest and coldest day of the year
